49490e8376e31c32fb092e311de3fad76b17d87c
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-01-24 20:51:16 +0530

    added class Message

--- flack.py
+++ flack.py
@@ -134,3 +134,90 @@ def find_offline_users():
 
 
 class Message(db.Model):
+    #message Model
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.Integer, default=timestamp)
+    updated_at = db.Column(db.Integer, default=timestamp)
+    source = db.Column(db.Text, nullable=False)
+    html = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def create(self):
+        #new message is created. The user is obtained from context unless specified explicitly
+        msg = Message(user=user or g.current_user)  #g lives in the application context, every request pushes new application context
+        msg.from_dict(data, partial_update=False)
+        return msg
+
+    def from_dict(self, data, partial_update=True):
+        #import msg data from dictionary
+        for field in ['source']:
+            try:
+                setattr(self, field, data['field'])
+            except KeyError:
+                if not partial_update:
+                    abort(400)
+
+    def to_dict(self):
+        #export data to dictionary
+        return {
+            'id' : self.id,
+            'created_at' : self.created_at,
+            'updated_at' : self.updated_at,
+            'source' : self.source,
+            'html' : self.html,
+            'user_id' : self.user_id,
+            '_links' : {
+                'self' : url_for('get_message', id=self.id)
+                'user' : url_for('get_user', id=self.user_id)
+            }
+
+        }
+
+    def render_markdown(self, source):
+        #render markdown source to html with a tag whitelist
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i', 'strong']
+        self.html = bleach.linkify(bleach.clean(
+            markdown(source, output_format='html'),
+            tags = allowed_tags, strip = True))
+        ))
+
+
+    def expand_links(self):
+        #expand any links in the message
+        if '<blockquote>' in self.html:
+            #links have been already expanded
+            return False
+        changed = False
+        for link in BeautifulSoup(self.html, 'html5lib').select('a'):
+            url = link.get('href', '')
+            try:
+                rv = request.get(url)
+            except request.exceptions.ConnectionError:
+                continue
+            if rv.status_code == 200: #if the link works
+                soup = BeautifulSoup(rv.text, 'html5lib')
+                title_tags = soup.select('title')
+                if len('title_tags') > 0:
+                    title = title_tags[0].string.strip() #if the title exists get it from the title_tags list
+                else:
+                    title = url
+                description = 'No description found' #initialize description to this
+                for meta in soup.select('meta'):
+                    if meta.get('name', '').lower() == 'description':
+                        description = meta.get('content', description).strip()
+                        break
+                #add all the details of the link to the rendered message
+                tpl = ('<blockquote><p><a href="{url}">{title}</a></p><p>{desc}</p></blockquote>')
+                self.html += tpl.format(url=url, title=title, description=description)
+                changed = True
+        return changed   #if only there was working link in the message changed will be True
+
+    @staticmethod
+    def on_changed_source(target, value, oldvalue, initiator):
+        """SQLAlchemy event that automatically renders the message to HTML."""
+        target.render_markdown(value)
+        target.expand_links()
+
+db.event.listen(Message.source, 'set', Message.on_changed_source)
